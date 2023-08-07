b44eddbd743515189cbd5bf99b2ca00a1686bda1
ifreeai <139997034+ifreeai@users.noreply.github.com>
2023-08-07 20:40:56 +0800

    Add files via upload

---
+++ studyone.c
@@ -0,0 +1,306 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//1.顺序结构
+//2.选择结构
+//3.循环结构
+
+//int main()
+//{
+//	int age = 20;
+//	if (age < 18)
+//		printf("未成年\n");
+//	else
+//		printf("成年\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 99;
+//	if (age < 18)
+//		printf("未成年\n");
+//	else
+//	{
+// if (age >= 18 && age < 28)
+//	printf("青年\n");
+//else if (age >= 28 && age < 50)
+//	printf("壮年\n");
+//else if (age >= 50 && age < 90)
+//	printf("老年\n");
+//else if (age >= 90 && age < 100)
+//	printf("老不死\n");
+//else if (age >= 100)
+//printf("入土\n");
+//	}
+//	/*else if (age >= 18 && age < 28)
+//		printf("青年\n");
+//	else if (age >= 28 && age < 50)
+//		printf("壮年\n");
+//	else if (age >= 50 && age < 90)
+//		printf("老年\n");
+//	else if(age >= 90 && age < 100)
+//		printf("老不死\n");
+//	else if(age >= 100)
+//		printf("入土\n");
+//	return 0;*/
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 0)
+//	{
+//		if (b == 1)
+//			printf("hehe\n");
+//	}
+//    else
+//		printf("haha");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 4;
+//	if (5==num)
+//	{
+//		printf("呵呵\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息日\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 1;
+//	int m = 2;
+//	switch (n)
+//	{
+//	case 1:m++;
+//	case 2:n++;
+//	case 3:
+//		switch (n)
+//		{
+//		case 1:n++;
+//		case 2:m++;
+//			   n++;
+//			break;
+//		}
+//	case 4:m++;
+//		break;
+//	default:
+//		break;
+//	}
+//	printf("m=%d,n=%d\n",m,n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	while(1)
+//		printf("hehe\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//  {
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+int main()
+{
+	int ch = getchar();
+	putchar(ch);
+	printf("%c\n", ch);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
