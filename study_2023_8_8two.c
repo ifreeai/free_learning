811f23e61ec89518a040d246f442d24757401973
ifreeai <139997034+ifreeai@users.noreply.github.com>
2023-08-08 17:29:26 +0800

    Add files via upload

---
+++ study_2023_8_8two.c
@@ -0,0 +1,97 @@
+//循环语句的学习2023年8月8日two
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//while循环语句：
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		i++;
+//	}
+//		return 0;
+//}	
+
+
+//for循环语句：
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//
+//	}
+//	return 0;
+//}
+
+
+//笔试题 
+//求其循环多少次？（嘿嘿）
+//int main()
+//{
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0, k = 0; k = 0; i++, k++)
+//		k++;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	while (i <= 10);
+//	return 0;
+//}
+int main()
+{
+	int n = 0;
+	int i = 0;
+	int ret = 1;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		ret = ret * i;
+	}
+	printf("ret = %d\n", ret);
+	return 0;
+}
+
