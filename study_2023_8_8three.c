47d183d260e2f3db0154d61e5ea2e88fc75a275a
ifreeai <139997034+ifreeai@users.noreply.github.com>
2023-08-08 22:08:03 +0800

    Add files via upload

---
+++ study_2023_8_8three.c
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//计算n的阶乘
+//int main()
+//{
+//	int n = 0;
+//	int ret = 1;
+//	int i = 0;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//计算1到10的阶乘之和
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		int ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//计算1到10的阶乘之和（简化升级版）
+//int main()
+//{
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = ret * n;
+//		sum = sum + ret;
+//	}
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+//写一个代码，在arr数组（有序）中找到7
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了，下标是：%d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到\n");
+//	return 0;
+//}
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);//计算元素个数
+	int left = 0;//左下标
+	int right = sz - 1;//右下标
+	
+	while (left<=right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
