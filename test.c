c61f5173058e169fec704fea91b0c9ad2b43f994
ifreeai <139997034+ifreeai@users.noreply.github.com>
2024-01-15 21:04:52 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}
+//	return 0;
+//}
+
+//用于调试
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ",10 - i);
+//	}
+//
+//	return 0;
+//}
+
+//int add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+
+//int main()
+//{
+//	printf("hehe\n");
+//	int a = 20;
+//	int b = 10;
+//	int c = add(a,b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;//保存最终结果
+//	int n = 0;
+//	int ret = 1;//保存n的阶乘
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int ret = 1;
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
