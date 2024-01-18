9c4133f5610e995fce895090dd9972efdd80e90b
ifreeai <139997034+ifreeai@users.noreply.github.com>
2024-01-18 16:20:24 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,128 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//数组指针的学习
+
+//arr-首元素地址
+//&arr[0]-首元素地址
+//&arr-数字的地址
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9 };
+//	int(*p)[10] = &arr;//p就是数组指针
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* arr[5] = { 0 };
+//	char* (*pa)[5] = &arr;
+//	return 0;
+//}
+
+//指针传参的学习
+
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d\n", *(p + i));
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9 };
+//	int* p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(p, sz);//一级指针P传参
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("%d\n", Add(a, b));
+//	//&函数名 和 函数名 都是函数的地址
+//	printf("%p\n", &Add);
+//	printf("%p\n", Add);
+//	return 0;
+//}
+
+//函数指针--是指向函数的指针--存放函数地址的一个指针
+
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 20;
+//	int b = 10;
+//	int (*pa)(int, int) = Add;//函数指针
+//	printf("%d\n", (*pa)(a, b));
+//	return 0;
+//}
+//
+//
+//void Print(char* str)
+//{
+//	printf("%s\n", str);
+//}
+//int main()
+//{
+//	void (*p)(char*) = Print;//函数指针
+//	(*p)("hello learning");
+//	return 0;
+//}
+
+
+//函数指针数组
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x,int y)
+//{
+//	return x * y;
+//}
+//int Div(int x,int y)
+//	{
+//	return x / y;
+//	}
+//int main()
+//{
+//	int* arr[5];//指针数组
+//	int (*pa)(int, int) = Add;//函数指针
+//	//需要一个数组，这个数组可以存放4个函数的地址 - 函数指针的数组
+//	int (*parr[4])(int, int) = { Add,Sub,Mul,Div };//函数指针的数组
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d\n", parr[i](2, 3));//5,-1,6,0
+//	}
+//	return 0;
+//}
+
+//简单练习
+
+//char* my_strcpy(char* dest, const char* src);
+//1.写一个函数指针 pf ，能够指向my_strcpy:
+//(char*)(*pf)(char*, const char*) = my_strcpy;
+//2.写一个函数指针数组 pfArr ,能够存放4个my_strcpy函数地址:
+//(char*)(*pfArr[4])(char*, const char*) = { mystrcpy,mystrcpy ,mystrcpy ,mystrcpy };
