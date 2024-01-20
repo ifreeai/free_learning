17f074d81d1ecc94cd50b3e3702083731f97c49b
ifreeai <139997034+ifreeai@users.noreply.github.com>
2024-01-20 20:46:25 +0800

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,19 @@
+//sit：鹏哥第34集（指针详解）
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//回调函数
+//void print(char* str)//print就是回调函数
+//{
+//	printf("hello:%s", str);
+//}
+//void test(void(*p)(char*))
+//{
+//	printf("test\n");
+//	p("bit");
+//}
+//int main()
+//{
+//	test(print);
+//	return 0;
+//}
+
