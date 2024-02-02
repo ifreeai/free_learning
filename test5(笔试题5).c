61d9c2a9301e036b23d92a2c900b3fae9a01073b
ifreeai <139997034+ifreeai@users.noreply.github.com>
2024-02-02 15:33:37 +0800

    Update test5(笔试题5).c

--- test5(笔试题5).c
+++ test5(笔试题5).c
@@ -4,7 +4,7 @@
 int main()
 {
 	int a[5][5];
-	int(*p)[4];//Êý×éÖ¸Õë
+	int(*p)[4];//æ•°ç»„æŒ‡é’ˆ
 	p = a;
 	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);//FFFFFFFFFFFFFFFC,-4
 	return 0;                                                   //p[4][2] == *(*(p+4)+2)
